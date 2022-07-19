3e3312e6cf59833ae347e27212e65caa1e1978cf
Wendell Modesto Silva <wendellsilva@Mac-mini-de-Wendell.local>
2022-07-19 13:08:01 -0300

    add auth in memory

--- src/main/java/br/com/wmoddev/examplesecurity/config/security/WebSecurityConfig.java
+++ src/main/java/br/com/wmoddev/examplesecurity/config/security/WebSecurityConfig.java
@@ -1,12 +1,20 @@
 package br.com.wmoddev.examplesecurity.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+    private final PasswordEncoder passwordEncoder;
+
+    public WebSecurityConfig(final PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -16,6 +24,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.anyRequest().authenticated()
 				.and()
 				.csrf().disable();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("wendell")
+			.password(passwordEncoder.encode("$teste_123"))
+			.roles("ADMIN");
 	}
 
 }
