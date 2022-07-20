302b83d37994d1bd5ff26388c562a689f4d47657
Wendell Modesto Silva <wendellsilva@Mac-mini-de-Wendell.local>
2022-07-20 20:10:53 -0300

    refactor WebSecurityConfig using SecurityFilterChain

--- src/main/java/br/com/wmoddev/examplesecurity/config/security/WebSecurityConfig.java
+++ src/main/java/br/com/wmoddev/examplesecurity/config/security/WebSecurityConfig.java
@@ -1,26 +1,15 @@
 package br.com.wmoddev.examplesecurity.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
-    private final PasswordEncoder passwordEncoder;
-    private final UserDetailsService userDetailsService;
+public class WebSecurityConfig {
 
-    public WebSecurityConfig(final PasswordEncoder passwordEncoder,
-    						 final UserDetailsService userDetailsService) {
-        this.passwordEncoder = passwordEncoder;
-        this.userDetailsService = userDetailsService;
-    }
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 				.httpBasic()
 				.and()
@@ -29,12 +18,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.anyRequest().authenticated()
 				.and()
 				.csrf().disable();
-	}
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService)
-			.passwordEncoder(passwordEncoder);
+		
+		return http.build();
 	}
 
 }
